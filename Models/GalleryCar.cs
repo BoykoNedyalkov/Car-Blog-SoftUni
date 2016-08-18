850c3e6ac53067c40c5895bd12b3a8d1957da3a1
BoykoNedyalkov <spoonmanxy@gmail.com>
2016-08-18 15:31:39 +0300

    Blog base with Gallery Functionality

---
+++ Models/GalleryCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBlog.Models
+{
+    public class GalleryCar
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string URL { get; set; }
+
+        public string Description { get; set; }
+    }
+}
